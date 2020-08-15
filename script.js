08db13584cbcbe574639a62a21513bda84555749
satyalrakesh <58408935+satyalrakesh@users.noreply.github.com>
2020-08-15 17:59:30 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+(function (window) {
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i in names) {
+
+  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+
+  if (firstLetter == 'j') {
+     ;
+     console.log("Bye "+names[i]);
+  } else {
+     console.log ("Hello "+names[i]);
+  }
+}
+})(window);
